e421abc20c7f01f2c47a4c768806a14bd165377a
unknown <otti55>
2022-10-05 09:27:21 -0500

    Cambioc category

---
+++ src/main/java/ciclo3/ciclo3/Repository/CategoryRepository.java
@@ -0,0 +1,31 @@
+package ciclo3.ciclo3.Repository;
+
+import ciclo3.ciclo3.Repository.crudRepository.CategoryCrudRepository;
+import ciclo3.ciclo3.entidades.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CategoryRepository {
+
+    @Autowired
+    private CategoryCrudRepository categoryCrudRepository;
+
+    public List<Category> getAll(){
+        return (List<Category>) categoryCrudRepository.findAll();
+    }
+
+    public Optional<Category> getCategory(int id){
+        return categoryCrudRepository.findById(id);
+    }
+
+    public Category save(Category c){
+        return categoryCrudRepository.save(c);
+    }
+    public void delete(Category c){
+        categoryCrudRepository.delete(c);
+    }
+
+
+}
---
+++ src/main/java/ciclo3/ciclo3/Repository/crudRepository/CategoryCrudRepository.java
@@ -0,0 +1,8 @@
+package ciclo3.ciclo3.Repository.crudRepository;
+
+
+import ciclo3.ciclo3.entidades.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryCrudRepository extends CrudRepository<Category,Integer> {
+}
